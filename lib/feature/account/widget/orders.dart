917a56e5a685b1ea90b22cecb65fe9959d110392
Hugo <hhugoac@gmail.com>
2023-06-19 10:05:59 -0600

    Product list vlayout added into the account page

--- lib/feature/account/widget/orders.dart
+++ lib/feature/account/widget/orders.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_clone/constants/global_variables.dart';
+import 'package:ecommerce_clone/feature/account/widget/single_product.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Orders extends StatefulWidget {
   const Orders({super.key});
@@ -9,6 +11,14 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
+  //Temporary list
+  List list = [
+    'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
+    'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
+    'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
+    'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,6 +47,22 @@ class _OrdersState extends State<Orders> {
                 style: TextStyle(
                   color: GlobalVariables.selectedNavBarColor,
                 ),
+              ),
+            ),
+            // Display your product list
+            Container(
+              height: 170,
+              padding: const EdgeInsets.only(
+                left: 10,
+                top: 20,
+                right: 0,
+              ),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  return SingleProduct(imageUrl: list[index]);
+                },
               ),
             ),
           ],
