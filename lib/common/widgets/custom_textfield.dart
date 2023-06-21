c28af88e0eca71ea1e0482ae083f92537dfcb32f
Hugo <hhugoac@gmail.com>
2023-06-21 10:42:35 -0600

    Add new Button sell. Modified the add_product_scren.dart to add the button into th bottom position in the layout of the UI

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  const CustomTextField(
-      {Key? key, required this.controller, required this.hintText})
-      : super(key: key);
+  final int maxLines;
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    this.maxLines = 1,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +31,7 @@ class CustomTextField extends StatelessWidget {
         }
         return null;
       },
+      maxLines: maxLines,
     );
   }
 }
--- lib/feature/admin/screen/add_product_screen.dart
+++ lib/feature/admin/screen/add_product_screen.dart
@@ -1,3 +1,6 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:ecommerce_clone/common/widgets/custom_button.dart';
+import 'package:ecommerce_clone/common/widgets/custom_textfield.dart';
 import 'package:ecommerce_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +13,30 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
+  TextEditingController productNameController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController quantityController = TextEditingController();
+
+  String category = 'Mobiles';
+
+  @override
+  void dispose() {
+    super.dispose();
+    productNameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+    quantityController.dispose();
+  }
+
+  List<String> productCategories = [
+    'Mobiles',
+    'Essentials',
+    'Books',
+    'Appliances',
+    'Fashions'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,29 +47,93 @@ class _AddProductScreenState extends State<AddProductScreen> {
             decoration:
                 const BoxDecoration(gradient: GlobalVariables.appBarGradient),
           ),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: Image.asset(
-                  'assets/images/amazon_in.png',
-                  width: 120,
-                  height: 45,
-                  color: Colors.black,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                child: const Text(
-                  'Admin',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+          title: const Text(
+            'Add product',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10).copyWith(top: 20),
+            child: Column(
+              children: [
+                DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.folder_open,
+                          size: 40,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'Selected Images',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey.shade400,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+                const SizedBox(height: 30),
+                CustomTextField(
+                    controller: productNameController,
+                    hintText: 'Product name'),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: descriptionController,
+                  hintText: 'Description name',
+                  maxLines: 7,
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(controller: priceController, hintText: 'Price'),
+                const SizedBox(height: 10),
+                CustomTextField(
+                    controller: quantityController, hintText: 'Quantity name'),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    value: category,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: productCategories.map((String item) {
+                      return DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      );
+                    }).toList(),
+                    onChanged: (String? newVal) {
+                      setState(() {
+                        category = newVal!;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomButton(text: 'Sell', onTap: () {}),
+              ],
+            ),
           ),
         ),
       ),
